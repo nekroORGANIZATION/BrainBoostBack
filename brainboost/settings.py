2573084f443e5db86e4be40a199f7fd222bac195
nekroMAYS <106478287+NaviOldManTop@users.noreply.github.com>
2025-09-20 00:46:10 +0300

    Update settings.py

--- brainboost/settings.py
+++ brainboost/settings.py
@@ -157,6 +157,10 @@ REST_FRAMEWORK = {
 # CORS
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  # Next.js
+    "https://brainboost.pp.ua/api/",
+    "https://brainboost.pp.ua/",
+    "http://172.17.10.23:3000",
+    "http://172.17.10.22:3000",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
